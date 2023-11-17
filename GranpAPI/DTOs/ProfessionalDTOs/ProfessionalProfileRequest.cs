b4d94127c0a9137536ad6b4dc4e16cb77cd862dc
Antonio Pio Maggio <a.maggio4@studenti.poliba.it>
2023-11-17 18:30:19 +0100

    Updated Professional DTOs

--- GranpAPI/DTOs/ProfessionalDTOs/ProfessionalProfileRequest.cs
+++ GranpAPI/DTOs/ProfessionalDTOs/ProfessionalProfileRequest.cs
@@ -1,7 +1,27 @@
+using Granp.Models.Enums;
+using Granp.Models.Types;
+
 namespace Granp.DTOs
 {
     public class ProfessionalProfileRequest
     {
-        // TODO DEFINE PROPERTIES
+        // User Info
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string PhoneNumber { get; set; } = null!;
+        
+        // Profile Info
+        public string? Description { get; set; }
+        public Profession Profession { get; set; }
+        public Address Address { get; set; } = null!;
+        public DateTime BirthDate { get; set; }
+        public string IdCardNumber { get; set; } = null!;
+        
+        // Job Info
+        public double HourlyRate { get; set; }
+        public int MaxDistance { get; set; }
+        public bool LongTimeJob { get; set; }
+        public bool ShortTimeJob { get; set; }
     }
 }
--- GranpAPI/DTOs/ProfessionalDTOs/ProfessionalPublicResponse.cs
+++ GranpAPI/DTOs/ProfessionalDTOs/ProfessionalPublicResponse.cs
@@ -1,7 +1,27 @@
+using Granp.Models.Enums;
+using Granp.Models.Types;
+
 namespace Granp.DTOs
 {
     public class ProfessionalPublicResponse
     {
-        // TODO DEFINE PROPERTIES
+        // User Info
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!; // Is this needed?
+        public string PhoneNumber { get; set; } = null!;
+        
+        // Profile Info
+        public string? Description { get; set; }
+        public Profession Profession { get; set; }
+        public Address Address { get; set; } = null!;
+        public int Age { get; set; }
+        public bool IsVerified { get; set; }
+        
+        // Job Info
+        public double HourlyRate { get; set; }
+        public int MaxDistance { get; set; }
+        public bool LongTimeJob { get; set; }
+        public bool ShortTimeJob { get; set; }
     }
 }
