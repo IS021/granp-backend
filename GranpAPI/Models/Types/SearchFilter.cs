46f35b75748f6d9eca937154979ff52c5e3edfb8
Antonio Pio Maggio <a.maggio4@studenti.poliba.it>
2023-12-09 17:47:05 +0100

    Update SearchFilter.cs

--- GranpAPI/Models/Types/SearchFilter.cs
+++ GranpAPI/Models/Types/SearchFilter.cs
@@ -5,13 +5,12 @@ namespace Granp.Models.Types
 {
     public class SearchFilter
     {
-        public Location Location { get; set; } = null!;
         public Profession? Profession { get; set; }
         public TimeSlot TimeSlot { get; set; }
         public double? MaxHourlyRate { get; set; }
+        public List<Gender?> Genders { get; set; }
         public bool? LongTimeJob { get; set; }
         public bool? ShortTimeJob { get; set; }
-        public int? MaxWeeksInAdvance { get; set; }
         public float? MinRating { get; set; }
         public int? MinAge { get; set; }
         public int? MaxAge { get; set; }
